450e822496b6366c7b962f7124c78b5a5e3a44c6
DivanoRizqiA <divanorizqianandra@student.uir.ac.id>
2024-03-22 10:41:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    Hello world <br>
+    aduh
+  </h1>
+  <Buttonc />
+</template>
+
+<script setup>
+import Buttonc from "./components/Buttonc.vue"
+</script>
---
+++ src/components/Buttonc.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="btn">
+        Submit
+    </button>
+</template>
+<style scoped>
+.btn {
+    background-color: black;
+    color: aqua;
+    font-size: 60px;
+}
+</style>
